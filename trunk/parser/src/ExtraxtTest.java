a46366f63d74d31221a90172c1003064b9656d43
altimerk@gmail.com <altimerk@gmail.com@3475a04d-e6d1-fc25-4c22-c2e993233281>
2012-08-03 07:05:49 +0000

    Изменения в структуре классов

--- trunk/parser/src/ExtraxtTest.java
+++ trunk/parser/src/ExtraxtTest.java
@@ -12,54 +12,57 @@ import java.net.*;
 
 
 public class ExtraxtTest {
-   
-public void doSome(String[] args) throws MalformedURLException, IOException{
-    String sourceUrlString="http://en.wikipedia.org/wiki/Singular_value_decomposition";
-    //System.setProperty("http.proxyHost", "proxy.stusta.mhn.de");
-    //System.setProperty("http.proxyPort", "3130");
-    //System.getProperties().put("proxySet", "true");     
-    if (args.length==0)
-      System.err.println("Using default argument of \""+sourceUrlString+'"');
-    else
-        sourceUrlString=args[0];
-    if (sourceUrlString.indexOf(':')==-1) sourceUrlString="file:"+sourceUrlString;
-    MicrosoftConditionalCommentTagTypes.register();
-    PHPTagTypes.register();
-    PHPTagTypes.PHP_SHORT.deregister(); // remove PHP short tags for this example otherwise they override processing instructions
-    MasonTagTypes.register();
-    Source source=new Source(new URL(sourceUrlString));
+   public List<Element> extractLinksFromPage(){
+	    String sourceUrlString="http://en.wikipedia.org/wiki/Singular_value_decomposition";
+	    System.setProperty("http.proxyHost", "192.168.20.4");
+	    System.setProperty("http.proxyPort", "3128");
+	    System.getProperties().put("proxySet", "true");    
+	    Authenticator.setDefault(new ProxyAuthenticator("tsretail\\sitroniks", "123qwE"));  
 
-    // Call fullSequentialParse manually as most of the source will be parsed.
-    source.fullSequentialParse();
+	    if (sourceUrlString.indexOf(':')==-1) sourceUrlString="file:"+sourceUrlString;
+	    MicrosoftConditionalCommentTagTypes.register();
+	    PHPTagTypes.register();
+	    PHPTagTypes.PHP_SHORT.deregister(); // remove PHP short tags for this example otherwise they override processing instructions
+	    MasonTagTypes.register();
+	    Source source = null;
+		try {
+			source = new Source(new URL(sourceUrlString));
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-    System.out.println("Document title:");
-    String title=getTitle(source);
-//    System.out.println(title==null ? "(none)" : title);
+	    // Call fullSequentialParse manually as most of the source will be parsed.
+	    source.fullSequentialParse();
 
-    System.out.println("\nDocument description:");
-    String description=getMetaValue(source,"description");
-//    System.out.println(description==null ? "(none)" : description);
+//	    System.out.println("Document title:");
+//	    String title=getTitle(source);
+//	    System.out.println(title==null ? "(none)" : title);
 
-    System.out.println("\nDocument keywords:");
-    String keywords=getMetaValue(source,"keywords");
-//    System.out.println(keywords==null ? "(none)" : keywords);
+//	    System.out.println("\nDocument description:");
+//	    String description=getMetaValue(source,"description");
+//	    System.out.println(description==null ? "(none)" : description);
 
-////    System.out.println("\nLinks to other documents:");
-    List<Element> linkElements=source.getAllElements(HTMLElementName.A);
+//	    System.out.println("\nDocument keywords:");
+//	    String keywords=getMetaValue(source,"keywords");
+//	    System.out.println(keywords==null ? "(none)" : keywords);
+	    List<Element> linkElements=source.getAllElements(HTMLElementName.A);
+	   return linkElements;
+   }
+public static void main(String[] args) throws MalformedURLException, IOException{
+	List<Element> linkElements = new ExtraxtTest().extractLinksFromPage();
+    for (Element linkElement : linkElements) 
+		System.out.println(linkElement.getAttributeValue("href"));
+		
+    
     UrlGrabber u = new UrlGrabber();
-    u.filterLinks(linkElements);
-////    new UrlGrabber().filterLinks(linkElements);
+    u.writeLinksToDB(linkElements);
+/*
     
     
-    System.out.println("\nLinks to other documents:");
-    //List<Element> linkElements=source.getAllElements(HTMLElementName.A);
-    for (Element linkElement : linkElements) {
-        String href=linkElement.getAttributeValue("href");
-        if (href==null) continue;
-        // A element can contain other tags so need to extract the text from it:
-        String label=linkElement.getContent().getTextExtractor().toString();
-        System.out.println(href);
-    }
 
 
 
@@ -99,7 +102,7 @@ ArrayList<String>l_snt = new SplitInSents().split(source.getTextExtractor().setI
 
 
 //writeToDB(l_snt);
-writeToDB(res_coll);
+new ExtraxtTest().writeToDB(res_coll);
 
 //for(String key: res_freq.keySet()){
 //	System.out.println(key + " " + res_freq.get(key));
@@ -109,7 +112,7 @@ writeToDB(res_coll);
 for (int i=0;i<l_snt.size();i++){
 	//System.out.println(l_snt.get(i));
 }
-	
+	*/
     
 }
 
@@ -148,11 +151,7 @@ public HashMap countFreq(ArrayList<String> al){
 	}
 
 
-    public static void main(String[] args) throws Exception {
-        new ExtraxtTest().doSome(args);
-    	
-    	
-  }
+
 
     private static String getTitle(Source source) {
         Element titleElement=source.getFirstElement(HTMLElementName.TITLE);
@@ -189,7 +188,7 @@ public HashMap countFreq(ArrayList<String> al){
 //	       String url = "jdbc:oracle:thin:@//localhost:1521/XE"; //host_name:port_number/service_name
 	       //String username = "word";
 	       //String password = "word";
-	       Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE", "word", "word");
+	       Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@oracle-sb.tsretail.ru:1551/TEST", "word", "word");
 	       //("jdbc:oracle:oci8:@xe", "word", "word");
 	       
 	       
@@ -229,7 +228,18 @@ public HashMap countFreq(ArrayList<String> al){
     
     
     
-    
+   static class ProxyAuthenticator extends Authenticator {
+	    private PasswordAuthentication auth;
+
+	    private ProxyAuthenticator(String user, String password) {
+	        auth = new PasswordAuthentication(user, password == null ? new char[]{} : password.toCharArray());
+	    }
+
+	    protected PasswordAuthentication getPasswordAuthentication() {
+	        return auth;
+	    }
+	}
+  
     
     
 }
--- trunk/parser/src/UrlGrabber.java
+++ trunk/parser/src/UrlGrabber.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -10,9 +11,31 @@ import java.util.regex.Pattern;
 import net.htmlparser.jericho.Element;
 
 public class UrlGrabber {
+	private Connection connection = null;
 
-	public void filterLinks(List<Element> links) {
-		for (Element linkElement : links) {
+	public UrlGrabber() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			connection = DriverManager.getConnection(
+					"jdbc:oracle:thin:@oracle-sb.tsretail.ru:1551/TEST",
+					"word", "word");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void writeLinksToDB(List<Element> links) {
+			String ins = null;
+			
+	       ins = "insert into url_links(url_id, link, last_check) values(URL_LINKS_SEQ.nextval,?,sysdate)";
+	       PreparedStatement stmt = null;
+	       try {
+			stmt = connection.prepareStatement(ins);
+			
+	       for (Element linkElement : links) {
 			String href = linkElement.getAttributeValue("href");
 			if (href == null)
 				continue;
@@ -20,57 +43,26 @@ public class UrlGrabber {
 			Pattern wiki = Pattern.compile("/wiki/[^:]+");
 			Matcher m = wiki.matcher(href);
 			if (m.matches()) {
-				System.out.println("http://en.wikipedia.org" + href);
-				writeToDB("http://en.wikipedia.org" + href);
+				try{
+				stmt.setString(1, "http://en.wikipedia.org" + href);
+				stmt.execute();
+				}
+				catch (SQLException e) {
+					// TODO Auto-generated catch block
+					  System.err.println(ins);
+					e.printStackTrace();
+				}
 			}
-
-		}
-	}
+	       }
+				stmt.close();
+				connection.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				  System.err.println(ins);
+				e.printStackTrace();
+			}
 		
-	   public void writeToDB(String s){
 
-		   String ins = null;
-		   
-		   try {
-		       // Load the JDBC driver
-		       String driverName = "oracle.jdbc.driver.OracleDriver";
-		       Class.forName(driverName);
-
-		       // Create a connection to the database
-		       
-		       String serverName = "127.0.0.1";
-		       String portNumber = "1521";
-		       String sid = "XE";
-//		       String url = "jdbc:oracle:thin:@//localhost:1521/XE"; //host_name:port_number/service_name
-		       String username = "word";
-		       String password = "word";
-		       Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@alexander-PC:1521/XE", "word", "word");
-		       //("jdbc:oracle:oci8:@xe", "word", "word");
-		       
-		       
-		       
-		       Statement stmt = connection.createStatement();
-//		       for (int i=0;i<al.size();i++){
-		    
-//		    		System.out.println(res_coll.get(i));
-		       ins = "insert into links(link, last_check) values(" + s + "," + "";
-		       try{
-		       stmt.execute(ins); }
-		       catch (SQLException e) {
-				   System.err.println(ins);
-				   e.printStackTrace();
-		       }
-		       
-		       stmt.close();
-		       connection.close();
-		   } catch (ClassNotFoundException e) {
-			   e.printStackTrace();
-		       // Could not find the database driver
-		   } catch (SQLException e) {
-			   System.err.println(ins);
-			   e.printStackTrace();
-		       // Could not connect to the database
-		   }
-	   }
-	   
+		
+	}   
 }
